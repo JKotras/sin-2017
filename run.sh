7783e9b05cf2f1b5bd37e2f306d1cbd7f2c4ddba
Honza Kotras <kotras@izon.cz>
2017-11-27 18:39:34 +0100

    Ping Agent

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath out:libs/jade.jar jade.Boot -gui -local-host 127.0.0.1 -agents test:sin.sin2017.project.agents.PingAgent
+java -classpath out:libs/jade.jar jade.Boot -gui -local-host 127.0.0.1 -agents test:sin.sin2017.project.agents.WorldAgent
--- src/sin/sin2017/project/agents/WorldAgent.java
+++ src/sin/sin2017/project/agents/WorldAgent.java
@@ -1,11 +1,10 @@
 package sin.sin2017.project.agents;
 
-import sun.management.Agent;
 
 public class WorldAgent extends jade.core.Agent {
 
     @Override
     protected void setup() {
-        super.setup();
+        System.out.println("Hi, my programmer");
     }
 }
